30efccf718b558d9c654a960a18c88b846fa58cc
ngCuong <stu755105020@hnue.edu.vn>
2026-09-23 14:18:22 +0700

    #hàm

--- t2/b2.cpp
+++ t2/b2.cpp
@@ -1,6 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+bool chua6(int n)
 {
-    cout << " sửa đổi lần 1";
+    while (n > 0)
+    {
+        if (n % 10 == 6)
+            return true;
+        else
+            return false;
+        n /= 10;
+    }
+}
+
+bool tongchan(int n)
+{
+    int tong = 0;
+    while (n > 0)
+    {
+        /* code */
+        tong += n % 10;
+        n /= 10;
+    }
+    return tong % 2 == 0;
+}
+bool chanle(int n)
+{
+    int c = 0, l = 0;
+    while (n > 0)
+    {
+        if (n % 2 == 0)
+            c++;
+        else
+            l++;
+        return l > c;
+    }
+}
+int mani()
+{
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if (chua6(i) && tongchan(i) && chanle(i))
+        {
+            cout << i << " ";
+        }
+    }
 }
